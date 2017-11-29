7febb39d0c265de15d03157f43fa5a723bd106f8
conti <contiki9@gmail.com>
2017-11-30 01:28:08 +0900

    存在しないJSの読込を消す

--- wp-content/themes/yui/functions.php
+++ wp-content/themes/yui/functions.php
@@ -19,7 +19,7 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'yui-styles', get_stylesheet_directory_uri() . '/css/style.css', array(), $the_theme->get( 'Version' ) );
     //wp_enqueue_script( 'jquery' );
 	//wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
-    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+    //wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
